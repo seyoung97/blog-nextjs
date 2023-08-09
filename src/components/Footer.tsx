8b51b27ec7925e069c6e108675df20a01286d7ef
seyoung97 <seyoung1st@gmail.com>
2023-08-09 20:51:02 +0900

    🎨Design: layout 스타일링

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <p className="">정세영 siiyong16@naver.com</p>;
+  return (
+    <p className="bg-slate-900 text-white py-2 text-sm text-center">
+      정세영 siiyong16@naver.com
+    </p>
+  );
 }
