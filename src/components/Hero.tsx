71a23a7528b4fa6ecf987f8e3cf5dcb9da27fae1
seyoung97 <seyoung1st@gmail.com>
2023-09-11 20:00:23 +0900

    Fix: myimage import 오류 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import profileImg from "/image/myimage.jpg";
+import profileImg from "/public/image/myimage.png";
 
 export default function Hero() {
   return (
