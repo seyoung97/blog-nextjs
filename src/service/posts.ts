7ccb12ad13fb77a9e396c47aa6de27d87128d4c5
seyoung97 <seyoung1st@gmail.com>
2023-09-12 10:22:48 +0900

    Refactor: 비즈니스 로직 필요 없는 주석 삭제

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -21,18 +21,9 @@ export const getAllPosts = cache(async () => {
   console.log("getAllPosts");
 
   const filePath = path.join(process.cwd(), "data", "posts.json");
-  // process.cwd()는 process가 현재 동작하고 있는 현재 경로를 받아오는 것을 의미
-  return (
-    readFile(filePath, "utf-8")
-      // readFile은 promise에 있는걸로 가져와야함
-      // 그래야 프로미스를 반환하는 readFile을 사용할 수 있음
-      .then<Post[]>(JSON.parse)
-      // .then(data => JSON.parse(data))의 축약버전임
-      // 전달하는 것과 인자가 같을 때 생략 가능
-      // <Post[]>라고 타입을 지정해줘야 아래 sort(a, d) 에서 a, d에 대한 타입 에러가 안남
-      .then((posts) => posts.sort((a, d) => (a.date > d.date ? -1 : 1)))
-  );
-  // 최신순으로 정렬
+  return readFile(filePath, "utf-8")
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.sort((a, d) => (a.date > d.date ? -1 : 1)));
 });
 
 export async function getFeaturedPosts(): Promise<Post[]> {
